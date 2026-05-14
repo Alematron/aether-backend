159334b0ef0ae4a0116fe16c28a4456ff1d1cdc9
Alexandra Schmuland <alexandraschmuland@gmail.com>
2026-05-14 14:38:39 +0000

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -177,7 +177,7 @@ async function start() {
 
     const port = process.env.PORT || 3000;
 
-    app.listen(port, () => {
+    app.listen(port, "0.0.0.0", () => {
       logger.info(`ÆTHER API running on port ${port}`);
       console.log(`Server running on port ${port}`);
     });
