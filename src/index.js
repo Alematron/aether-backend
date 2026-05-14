8cb5a13e8b9b53feb409bb75ab1a428e621d2f85
Alexandra Schmuland <alexandraschmuland@gmail.com>
2026-05-14 12:17:06 +0000

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -36,7 +36,18 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function(origin, callback) {
+    const allowed = [
+      'https://blueroom.club',
+      'https://www.blueroom.club',
+      process.env.FRONTEND_URL,
+    ].filter(Boolean)
+    if (!origin || allowed.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
